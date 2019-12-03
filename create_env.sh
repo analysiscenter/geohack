fdb0a6a8a5e66d024bfa8ca3bd99ffa3bfcec47a
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2019-12-03 19:16:28 +0300

    Scripts to run container

---
+++ create_env.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# install Docker
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# install NVIDIA docker version 2
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+ sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+# reload the Docker daemon configuration
+sudo systemctl restart docker
+
+# Test nvidia-smi with the latest official CUDA image
+sudo docker run --gpus all --rm nvidia/cuda nvidia-smi
+
+# set up directory for notebooks
+sudo mkdir /notebooks
+rm -rf ./notebooks
+ln -s /notebooks ./notebooks
+
+sudo docker pull analysiscenter1/geohack
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+notebooks_vol=${DS_NOTEBOOKS_DIR:-`pwd`/notebooks}
+config_vol=${DS_CONFIG_DIR:-`pwd`/config}
+secret_vol=${DS_SECRET_DIR:-`pwd`/secret}
+port=${DS_PORT:-8888}
+image=${DS_IMAGE:-dpodvyaznikov/geohack}
+
+
+sudo docker run --gpus all --rm -p ${port}:8888 \
+  -v ${notebooks_vol}:/notebooks \
+  -v ${config_vol}:/jupyter/config \
+  -v ${secret_vol}:/jupyter/secret \
+  $@ \
+  ${image}
